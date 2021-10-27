5be6017eaa1b9a0f06402239b4f558dd2c930ca3
jnabin <jahangirnabin2@gmail.com>
2021-10-27 21:52:27 +0600

    adding angular app

---
+++ JobPostWebApi/JobPostWebApi/Model/JobDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobPostWebApi.Model
+{
+    public class JobDetails
+    {
+    }
+}
